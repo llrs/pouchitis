fb54a5204255211aa75722a9c6f8de7d1111a37c
llrs <lluis.revilla@gmail.com>
2019-08-05 18:08:17 +0200

    Update preholidays

--- R/within.R
+++ R/within.R
@@ -31,64 +31,34 @@ otus <- filter_RNAseq(norm_RNAseq(otus))
 # MR_i <- cumNorm(MR_i, metagenomeSeq::cumNormStat(MR_i))
 # otus <- MRcounts(MR_i, norm = TRUE, log = TRUE)
 
-expr.data <- expr-apply(expr, 1, median)
-pca.expr <- prcomp(t(expr.data), center = FALSE, scale. = FALSE)
-s <- summary(pca.expr)
-plot(pca.expr$x, pch = 16, col = m$Location, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca.expr$x, pch = 16, col = m$Outcome, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca.expr$x, pch = 16, col = m$Batch, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca.expr$x, pch = 16, col = m$Gender, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-
-otus.data <- otus-apply(otus, 1, median)
-pca <- prcomp(t(otus.data), center = TRUE, scale. = TRUE)
-s <- summary(pca)
-plot(pca$x, pch = 16, col = m$Location, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca$x, pch = 16, col = m$Outcome, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca$x, pch = 16, col = m$Gender, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-plot(pca$x, pch = 16, col = m$Batch, 
-     xlab = paste("PC1", round(s$importance[2, 1]*100, 4), "%"),
-     ylab = paste("PC2", round(s$importance[2, 2]*100, 4), "%"))
-
 Demographics <- model_RGCCA(m, c("Gender", "ID_1", "Antibiotics"))
 Location <- model_RGCCA(m, c("Location", "ISCORE"))
 A <- list(RNAseq =  t(expr), "16S" = t(otus))
 shrinkage <- sapply(A, tau.estimate)
 C <- matrix(0, ncol = 2, nrow = 2, dimnames = list(names(A), names(A)))
 model0 <- subSymm(C, "RNAseq", "16S", 1)
+model0 <- subSymm(model0, "RNAseq", "RNAseq", 1)
 m0 <- sgcca(A, model0, c1 = shrinkage, ncomp = rep(2, length(A)))
 m0 <- improve.sgcca(m0, names(A))
-saveRDS(m0, "model0_edge.RDS")
+saveRDS(m0, "imodel0_edge.RDS")
 
 meta <- model_RGCCA(m, c("Gender", "ID_1", "Antibiotics", "Location", "ISCORE"))
 A1.2 <- A
 A1.2$meta <- meta
 C <- matrix(0, ncol = length(A1.2), nrow = length(A1.2), dimnames = list(names(A1.2), names(A1.2)))
 model1 <- subSymm(C, "RNAseq", "16S", 1)
+model1 <- subSymm(C, "RNAseq", "RNAseq", 1)
 model1 <- subSymm(model1, "RNAseq", "meta", 1)
 model1 <- subSymm(model1, "meta", "16S", 1)
 m1 <- sgcca(A1.2, model1, c1 = c(shrinkage, 1), ncomp = rep(2, length(A1.2)))
 m1 <- improve.sgcca(m1, names(A1.2))
 plot(m1$Y$RNAseq[, 1], m1$Y$`16S`[, 1], pch = 16, col = m$Location)
-saveRDS(m1, "model1_edge.RDS")
+saveRDS(m1, "imodel1_edge.RDS")
 
 model1.1 <- subSymm(model1, "RNAseq", "16S", 0)
 m1.1 <- sgcca(A1.2, model1.1, c1 = c(shrinkage, 1), ncomp = rep(2, length(A1.2)))
 m1.1 <- improve.sgcca(m1.1, names(A1.2))
-saveRDS(m1.1, "model1.1_edge.RDS")
+saveRDS(m1.1, "imodel1.1_edge.RDS")
 plot(m1.1$Y$RNAseq[, 1], m1.1$Y$`16S`[, 1], pch = 16, col = m$ISCORE)
 
 
@@ -100,24 +70,24 @@ Ab <- lapply(A1.2, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
 
 testing <- function(x, ...) {
   try({
-  result.sgcca <- RGCCA::sgcca(C = x, 
-                               verbose = FALSE, 
-                               scale = FALSE,
-                               ...)
-  analyze(result.sgcca)}, 
-  silent = TRUE)
+    result.sgcca <- RGCCA::sgcca(C = x, 
+                                 verbose = FALSE, 
+                                 scale = FALSE,
+                                 ...)
+    analyze(result.sgcca)}, 
+    silent = TRUE)
 }
 
 out <- sapply(designs1.2, testing, A = Ab, c1 = c(shrinkage, 1), USE.NAMES = FALSE)
 out <- as.data.frame(t(out))
-saveRDS(out, "model1.2_testing_edge.RDS")
+saveRDS(out, "imodel1.2_testing_edge.RDS")
 
 
 columns <- grep("var", colnames(out))
 model1.2 <- symm(model1.1, out[which.max(out$AVE_inner), columns])
 m1.2 <- sgcca(A1.2, model1.2, c1 = c(shrinkage, 1), ncomp = rep(2, 3))
 m1.2 <- improve.sgcca(m1.2, names(A1.2))
-saveRDS(m1.2, "model1.2_edge.RDS")
+saveRDS(m1.2, "imodel1.2_edge.RDS")
 
 # Models 2 ####
 
@@ -129,13 +99,14 @@ C <- matrix(
   dimnames = list(names(A2.2), names(A2.2))
 )
 model2 <- subSymm(C, "16S", "RNAseq", 1)
+model2 <- subSymm(C, "RNAseq", "RNAseq", 1)
 model2 <- subSymm(model2, "RNAseq", "Demographics", 1)
 model2 <- subSymm(model2, "RNAseq", "Location", 1)
 model2 <- subSymm(model2, "16S", "Demographics", 1)
 model2 <- subSymm(model2, "16S", "Location", 1)
 m2 <- sgcca(A2.2, model2, c1 = shrinkage2, ncomp = rep(2, length(A2.2)))
 m2 <- improve.sgcca(m2, names(A2.2))
-saveRDS(m2, "model2_edge.RDS")
+saveRDS(m2, "imodel2_edge.RDS")
 
 Ab <- lapply(A2.2, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
 designs2.1 <- weight_design(weights = 3, size = 4)
@@ -143,24 +114,24 @@ k <- vapply(designs2.1, correct, logical(1L))
 designs2.1 <- designs2.1[k]
 out <- sapply(designs2.1, testing, A = Ab, c1 = shrinkage2, USE.NAMES = FALSE)
 out <- as.data.frame(t(out))
-saveRDS(out, "model2.1_testing_edge.RDS")
+saveRDS(out, "imodel2.1_testing_edge.RDS")
 
 columns <- grep("var", colnames(out))
 model2.1 <- symm(model2, out[which.max(out$AVE_inner), columns])
 m2.1 <- sgcca(A2.2, model2.1, c1 = shrinkage2, ncomp = rep(2, 4))
 m2.1 <- improve.sgcca(m2.1, names(A2.2))
-saveRDS(m2.1, "model2.1_edge.RDS")
+saveRDS(m2.1, "imodel2.1_edge.RDS")
 
 designs2.2 <- weight_design(weights = 11, size = 4, 
                             diff0 = which(lower.tri(model2.1) & model2.1 != 0))
 out <- sapply(designs2.2, testing, A = Ab, c1 = shrinkage2, USE.NAMES = FALSE)
 out <- as.data.frame(t(out))
-saveRDS(out, "model2.2_testing_edge.RDS")
+saveRDS(out, "imodel2.2_testing_edge.RDS")
 
 model2.2 <- symm(C, out[which.max(out$AVE_inner), grep("var", colnames(out))])
-m2.2 <- sgcca(, model2.2, c1 = shrinkage2, ncomp = rep(2, 4))
-m2.2 <- improve.sgcca(m2.2, names())
-saveRDS(m2.2, "model2.2_edge.RDS")
+m2.2 <- sgcca(A2.2, model2.2, c1 = shrinkage2, ncomp = rep(2, 4))
+m2.2 <- improve.sgcca(m2.2, names(A2.2))
+saveRDS(m2.2, "imodel2.2_edge.RDS")
 
 if (model2.2["RNAseq", "16S"] == 0)  {
   out <- readRDS("model2.1_testing_edge.RDS")
@@ -173,13 +144,13 @@ if (model2.2["RNAseq", "16S"] == 0)  {
   out <- sapply(designs2.3, testing, A = Ab, c1 = shrinkage2, USE.NAMES = FALSE)
   out <- simplify2array(out[lengths(out) != 1])
   out <- as.data.frame(t(out))
-  saveRDS(out, "model2.3_testing_edge.RDS")
+  saveRDS(out, "imodel2.3_testing_edge.RDS")
   
   
   model2.3 <- symm(C, out[which.max(out$AVE_inner), grep("var", colnames(out))])
   m2.3 <- sgcca(A2.2, model2.3, c1 = shrinkage2, ncomp = rep(2, 4))
   m2.3 <- improve.sgcca(m2.3, names(A2.2))
-  saveRDS(m2.3, "model2.3_edge.RDS")
+  saveRDS(m2.3, "imodel2.3_edge.RDS")
 }
 
 l <- list.files(pattern = "model.*[0-9]_edge.RDS")
@@ -199,7 +170,7 @@ models[pos]
 s <- lapply(seq_along(l), function(x) {
   y <- readRDS(l[x])
   df <- data.frame("RNAseq" = y$Y[[1]][, 1], "16S" = y$Y[[2]][, 1], 
-             "ID" = 1:nrow(y$Y[[1]]), Model = models[x], check.names = FALSE)
+                   "ID" = 1:nrow(y$Y[[1]]), Model = models[x], check.names = FALSE)
   cbind(df, m[, c("Gender", "Location", "Outcome", "ISCORE", "Antibiotics")])
 })
 
@@ -228,6 +199,8 @@ ggplot(s3) +
   facet_wrap(~Model, nrow = 2, scales = "free")
 
 ## Create some index and permutations to test the variability on the models.
+
+
 
 # Boots ####
 seed <- 487129
@@ -278,11 +251,11 @@ m0 <- data.frame(inner = model0$AVE$AVE_inner[1],
                  outer = model0$AVE$AVE_outer[1],
                  Model = "0")
 m1.2 <- data.frame(inner = model1.2$AVE$AVE_inner[1], 
-                 outer = model1.2$AVE$AVE_outer[1],
-                 Model = "1.2")
+                   outer = model1.2$AVE$AVE_outer[1],
+                   Model = "1.2")
 m2.2 <- data.frame(inner = model2.2$AVE$AVE_inner[1], 
-                 outer = model2.2$AVE$AVE_outer[1],
-                 Model = "2.2")
+                   outer = model2.2$AVE$AVE_outer[1],
+                   Model = "2.2")
 ggplot(aves, aes(inner, outer, col = Model)) +
   geom_point(alpha = 0.25) +
   stat_ellipse() +
