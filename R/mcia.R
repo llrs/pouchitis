fa6d0293acff341826b91eaeea403d1661d6f0ac
llrs <lluis.revilla@gmail.com>
2022-05-16 17:25:10 +0200

    Save also for the thesis

--- R/mcia.R
+++ R/mcia.R
@@ -9,11 +9,19 @@ saveRDS(out, "mcia.RDS")
 meta <- read.delim("data/from_zip/metadata.txt", row.names = 1)
 m <- meta[rownames(out$mcoa$SynVar), ]
 data_plot <- cbind(out$mcoa$SynVar, m)
+theme_set(theme_bw())
+theme_update(strip.background = element_blank(), 
+             panel.grid.minor = element_blank(),
+             axis.text.x = element_blank(),
+             axis.text.y = element_blank(),
+             axis.ticks = element_blank())
 data_plot %>% 
   ggplot() +
   geom_point(aes(SynVar1, SynVar2, col = Location, shape = Location)) +
-  labs(title = "MCIA on the pouchitis dataset")
+  labs(title = "MCIA in the Morgan dataset")
 ggsave("Figures/mcia.png")
+ggsave("~/Documents/projects/thesis/images/morgan-mcia.png", width = 170,
+       units = "mm", dpi = 300, bg = "white")
 
 p1 <- data_plot %>% 
   ggplot() +
